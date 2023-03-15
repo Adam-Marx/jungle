83e50003a0e59df6908e0ee8b3430bd21b85577e
Adam-Marx <adamalexmarx@gmail.com>
2023-03-14 22:14:56 -0700

    Home page cypress testing

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+describe('Home Page', () => { // Provide a callback function here
+
+  it('loads the home page', () => { // Provide a callback function here
+
+    cy.visit('/')
+  })
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
